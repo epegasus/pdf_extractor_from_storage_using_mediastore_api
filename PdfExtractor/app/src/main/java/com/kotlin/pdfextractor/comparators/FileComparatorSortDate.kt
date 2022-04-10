13ec19752cbfa85283361f6bfd697228b0cca6e3
Sohaib Ahmed <100923337+sohaib-developer@users.noreply.github.com>
2022-04-10 15:02:32 +0500

    Delete PdfExtractor directory

--- PdfExtractor/app/src/main/java/com/kotlin/pdfextractor/comparators/FileComparatorSortDate.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlin.pdfextractor.comparators
-
-import com.kotlin.pdfextractor.models.FileItem
-
-class FileComparatorSortDate : Comparator<FileItem> {
-    override fun compare(p0: FileItem?, p1: FileItem?): Int {
-        return (p0!!.exactDateModified).compareTo(p1!!.exactDateModified)
-    }
-}
--- PdfExtractor/app/src/main/java/com/kotlin/pdfextractor/comparators/FileComparatorSortName.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlin.pdfextractor.comparators
-
-import com.kotlin.pdfextractor.models.FileItem
-
-class FileComparatorSortName : Comparator<FileItem> {
-    override fun compare(p0: FileItem?, p1: FileItem?): Int {
-        return (p0!!.fileName).compareTo(p1!!.fileName)
-    }
-}
